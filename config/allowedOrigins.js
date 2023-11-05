fe7bb1133fb893d11c4e3bf60aee787999291479
Darrel M Schreiner <59634018+dmschrein@users.noreply.github.com>
2023-11-05 15:24:17 -0800

    added server and middleware

---
+++ config/allowedOrigins.js
@@ -0,0 +1,7 @@
+const allowedOrigins = [
+    'http://localhost:3000',
+    'https://www.dandreparishop.com',
+    'https://danddrepairshop.com'
+]
+
+module.exports = allowedOrigins 
---
+++ middleware/logger.js
@@ -0,0 +1,29 @@
+const { format } = require('date-fns')
+const { v4: uuid } = require('uuid')
+const fs = require('fs')
+const fsPromises = require('fs').promises
+const path = require('path')
+
+const logEvents = async (message, logFileName) => {
+    const dateTime = `${format(new Date(), 'yyyyMMdd\tHH:mm:ss')}`
+    const logItem = `${dateTime}\t${uuid()}\t${message}\n`
+
+    // look for the directory containing log files or create a new one
+    try {
+        if (!fs.existsSync(path.join(__dirname, '..', 'logs'))) {
+            await fsPromises.mkdirSync(path.join(__dirname, '..', 'logs'))
+        }
+        await fsPromises.appendFile(path.join(__dirname, '..', 'logs', logFileName), logItem)
+
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+const logger = (req, res, next) => {
+    logEvents(`${req.method}\t${req.url}\t${req.headers.origin}`, 'reqLog.log')
+    console.log(`${req.method} ${req.path}`)
+    next()
+}
+
+module.exports = { logEvents, logger }
